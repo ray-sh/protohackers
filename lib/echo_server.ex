d046641961ed1ba5146d81cb970c4ea1a5622ec6
Ray Ding <rayding@outlook.com>
2023-02-04 21:31:44 +0800

    neet version

--- lib/echo_server.ex
+++ lib/echo_server.ex
@@ -8,13 +8,6 @@ defmodule EchoServer do
 
   @impl GenServer
   def init(_args) do
-    {:ok, %{lsocket: nil}, {:continue, :start_server}}
-  end
-
-  @impl GenServer
-  def handle_continue(:start_server, state) do
-    Logger.info("genserver start #{inspect(self())}")
-
     listen_options = [
       ifaddr: {0, 0, 0, 0},
       mode: :binary,
@@ -23,33 +16,40 @@ defmodule EchoServer do
       exit_on_close: false
     ]
 
-    {:ok, lsocket} = :gen_tcp.listen(5002, listen_options)
-    Logger.info("start to listen on port 5002")
-    {:noreply, %{state | lsocket: lsocket}, {:continue, :wait_client}}
+    case :gen_tcp.listen(5002, listen_options) do
+      {:ok, lsocket} ->
+        {:ok, %{lsocket: lsocket}, {:continue, :wait_connect}}
+
+      {:error, error} ->
+        Logger.error("can't listen on the port #{inspect(error)}")
+        {:stop, error}
+    end
   end
 
   @impl true
-  def handle_continue(:wait_client, state) do
-    Logger.info("wait for new client")
-    {:ok, socket} = :gen_tcp.accept(state.lsocket)
-    {:ok, msg} = read_msg(socket, "")
-    Logger.info("send received msg #{msg} back")
-    :gen_tcp.send(socket, msg)
-    :gen_tcp.close(socket)
-    {:noreply, state, {:continue, :wait_client}}
+  def handle_continue(:wait_connect, state) do
+    case :gen_tcp.accept(state.lsocket) do
+      {:ok, socket} ->
+        {:ok, msg} = read_msg(socket, "")
+        Logger.info("send received msg #{msg} back")
+        :gen_tcp.send(socket, msg)
+        :gen_tcp.close(socket)
+
+      {:error, error} ->
+        Logger.error("accept error #{inspect(error)}")
+    end
+
+    {:noreply, state, {:continue, :wait_connect}}
   end
 
   defp read_msg(socket, bs) do
-    Logger.info("wait client msg")
-
     case :gen_tcp.recv(socket, 0, 10_000) do
       {:ok, packet} ->
-        Logger.info("receive #{inspect(packet)}")
         read_msg(socket, [bs, packet])
 
       {:error, :closed} ->
         Logger.info("client closed")
-        {:ok, :erlang.list_to_binary(bs)}
+        {:ok, bs}
     end
   end
 end
